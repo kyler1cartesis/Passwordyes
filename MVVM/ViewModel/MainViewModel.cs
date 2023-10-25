86f8eb349b25fc981112e3bb5040f2eca0e22618
AndreyKoposov <140280539+AndreyKoposov@users.noreply.github.com>
2023-10-26 01:06:10 +0300

    Базовый начальный макет интерфейса

---
+++ MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Password_Manager.Core;
+
+namespace Password_Manager.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand DataBasesViewCommand { get; set; }
+
+        public HomeViewModel HomeVM { get; set; }
+        public DataBasesViewModel DataBasesVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get 
+            { 
+                return _currentView; 
+            }
+            set 
+            { 
+                _currentView = value; 
+                OnPropertyChanged(); 
+            }
+        }
+
+        public MainViewModel()
+        {
+            HomeVM = new HomeViewModel();
+            DataBasesVM = new DataBasesViewModel();
+            CurrentView = HomeVM;
+
+            HomeViewCommand = new RelayCommand(obj => 
+            {
+                CurrentView = HomeVM;
+
+            });
+
+            DataBasesViewCommand = new RelayCommand(obj =>
+            {
+                CurrentView = DataBasesVM;
+
+            });
+        }
+    }
+}
