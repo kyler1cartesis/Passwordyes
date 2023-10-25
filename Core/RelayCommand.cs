5ca9e1df5f9b9a8178730a3b78ef3b9ea01c7079
AndreyKoposov <140280539+AndreyKoposov@users.noreply.github.com>
2023-10-25 21:24:45 +0300

    Добавлены первые варианты кнопок меню, поиска, названия

--- Core/RelayCommand.cs
+++ Core/RelayCommand.cs
@@ -5,12 +5,12 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
-namespace Password_Manager.Utilities
+namespace Password_Manager.Core
 {
-    internal class RelayCommand : ICommand
+    class RelayCommand : ICommand
     {
-        private readonly Action<object> _execute;
-        private readonly Func<object, bool> _canExecute;
+        private Action<object> _execute;
+        private Func<object, bool> _canExecute;
 
         public event EventHandler CanExecuteChanged
         {
@@ -24,7 +24,15 @@ namespace Password_Manager.Utilities
             _canExecute = canExecute;
         }
 
-        public bool CanExecute(object parameter) => _canExecute == null || _canExecute(parameter);
-        public void Execute(object parameter) => _execute(parameter);
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
     }
+
 }
